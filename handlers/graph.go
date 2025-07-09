ab6367b45f85d0f9fa8fdd8d6b9c744ba8071e08
user <marcus.work005@gmail.com>
2025-07-09 20:26:27 +0700

    x

--- handlers/graph.go
+++ handlers/graph.go
@@ -7,7 +7,7 @@ import (
 	"text/template"
 
 	"github.com/gorilla/mux"
-	appsv1 "k8s.io/api/apps/v1"
+	// appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	netv1 "k8s.io/api/networking/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
