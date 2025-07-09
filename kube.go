e697dfea575e4f38cee508e1d1f5437eaa0ab338
user <marcus.work005@gmail.com>
2025-07-09 21:02:13 +0700

    x

--- kube.go
+++ kube.go
@@ -1,23 +1,105 @@
 package main
 
 import (
-	"log"
+	"context"
+	"fmt"
+	"strings"
 
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	netv1 "k8s.io/api/networking/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
-// GetClientset returns a Kubernetes clientset using kubeconfig.
-func GetClientset() *kubernetes.Clientset {
+func renderKubernetesOverview() (string, error) {
 	config, err := clientcmd.BuildConfigFromFlags("", clientcmd.RecommendedHomeFile)
 	if err != nil {
-		log.Fatalf("Failed to load kubeconfig: %v", err)
+		return "", fmt.Errorf("failed to load kubeconfig: %w", err)
 	}
 
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
-		log.Fatalf("Failed to create clientset: %v", err)
+		return "", fmt.Errorf("failed to create clientset: %w", err)
 	}
 
-	return clientset
+	ctx := context.TODO()
+	deployments, err := clientset.AppsV1().Deployments("default").List(ctx, metav1.ListOptions{})
+	if err != nil {
+		return "", fmt.Errorf("failed to list deployments: %w", err)
+	}
+
+	services, _ := clientset.CoreV1().Services("default").List(ctx, metav1.ListOptions{})
+	ingresses, _ := clientset.NetworkingV1().Ingresses("default").List(ctx, metav1.ListOptions{})
+	pods, _ := clientset.CoreV1().Pods("default").List(ctx, metav1.ListOptions{})
+
+	var sb strings.Builder
+	sb.WriteString(`<table>
+<thead><tr>
+<th>Deployment</th>
+<th>Pods</th>
+<th>Service</th>
+<th>Ingress</th>
+<th>Namespace</th>
+</tr></thead>
+<tbody>`)
+
+	for _, d := range deployments.Items {
+		selector := metav1.FormatLabelSelector(&metav1.LabelSelector{MatchLabels: d.Spec.Selector.MatchLabels})
+		var matchedPods []corev1.Pod
+		for _, p := range pods.Items {
+			if matchLabels(d.Spec.Selector.MatchLabels, p.Labels) {
+				matchedPods = append(matchedPods, p)
+			}
+		}
+
+		var serviceName, ingressName string
+		for _, s := range services.Items {
+			if matchLabels(d.Spec.Selector.MatchLabels, s.Spec.Selector) {
+				serviceName = s.Name
+				break
+			}
+		}
+
+		for _, i := range ingresses.Items {
+			for _, rule := range i.Spec.Rules {
+				if rule.HTTP == nil {
+					continue
+				}
+				for _, path := range rule.HTTP.Paths {
+					if path.Backend.Service != nil && path.Backend.Service.Name == serviceName {
+						ingressName = i.Name
+						break
+					}
+				}
+				if ingressName != "" {
+					break
+				}
+			}
+			if ingressName != "" {
+				break
+			}
+		}
+
+		sb.WriteString("<tr>")
+		sb.WriteString(fmt.Sprintf("<td>%s</td>", d.Name))
+		sb.WriteString(fmt.Sprintf("<td>%d</td>", len(matchedPods)))
+		sb.WriteString(fmt.Sprintf("<td>%s</td>", serviceName))
+		sb.WriteString(fmt.Sprintf("<td>%s</td>", ingressName))
+		sb.WriteString(fmt.Sprintf("<td>%s</td>", d.Namespace))
+		sb.WriteString("</tr>")
+	}
+
+	sb.WriteString("</tbody></table>")
+	return sb.String(), nil
+}
+
+func matchLabels(selector map[string]string, labels map[string]string) bool {
+	for k, v := range selector {
+		if labels[k] != v {
+			return false
+		}
+	}
+	return true
 }
--- main.go
+++ main.go
@@ -1,40 +1,26 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "text/template"
-
-    "github.com/gorilla/mux"
-    "k8s-dashboard/handlers" // 👈 เพิ่มตรงนี้
+	"log"
+	"net/http"
+	"text/template"
 )
 
-var templates *template.Template
+var templates = template.Must(template.ParseFiles("templates/index.html"))
 
 func main() {
-    // templates = template.Must(template.ParseGlob("templates/*.html"))
-	templates = template.Must(template.ParseFiles(
-		"templates/base.html",
-		"templates/index.html",
-		"templates/deployments.html",
-		"templates/graph.html",
-	))
-	
-    r := mux.NewRouter()
-    r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-
-    r.HandleFunc("/", indexHandler)
-    r.HandleFunc("/deployments", handlers.DeploymentsHandler) // 👈 ใช้จาก package
-    r.HandleFunc("/deployments/{name}", handlers.DeploymentGraphHandler)
-
-    log.Println("Server running on http://localhost:4000")
-    http.ListenAndServe(":4000", r)
+	http.HandleFunc("/", indexHandler)
+	log.Println("Server running on http://localhost:4000")
+	http.ListenAndServe(":4000", nil)
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-    err := templates.ExecuteTemplate(w, "base.html", nil)
-    if err != nil {
-        log.Printf("Render failed: %v", err)
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-    }
+	html, err := renderKubernetesOverview()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	templates.ExecuteTemplate(w, "index.html", map[string]interface{}{
+		"HTML": template.HTML(html),
+	})
 }
